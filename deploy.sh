69ca4aa01fa7eecdfcd11ba909d8b45d3226a9c8
Tim M. Madsen <tim@tmadsen.com>
2017-09-01 23:36:10 +0200

    Move around a bunch of files
    Trying to establish a bit of order.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ./generate-changelog.sh
 sudo -u www-data cp *.html /var/www/jwt.show
-sudo -u www-data cp *.css /var/www/jwt.show
+sudo -u www-data cp -r styles /var/www/jwt.show
 sudo -u www-data cp -r changelog /var/www/jwt.show
 sudo -u www-data cp -r script /var/www/jwt.show
 sudo -u www-data cp -r stats /var/www/jwt.show
