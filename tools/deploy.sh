125513dac79295b54ce7971ecda1877c1c87e228
Tim M. Madsen <tim@tmadsen.com>
2018-08-28 17:29:01 +0200

    Housecleaning: move/delete a couple of files
    The src folder now contains all the site files. The tools folder now
    holds all the tools. I don't really use ctags for this project, so the
    tags file is removed. So is a test file that I used for expirements with
    generating the changelog with escaped newlines and quotes.

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yarn
 rm -rf dist
-./tools/generate-changelog-entries.sh > ./src/models/Changes.js
+./generate-changelog-entries.sh > ../src/models/Changes.js
 yarn run build
 sudo -u www-data rm -rf /var/www/jwt.show/*
 sudo -u www-data cp dist/* /var/www/jwt.show
