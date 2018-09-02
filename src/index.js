5961e8e086a674ca15f7d1bb4ef61cf338f0befb
Tim M. Madsen <tim@tmadsen.com>
2018-09-02 15:58:56 +0200

    Make direct links decode again
    A bug had sneaked in during the refactor of `decodedJwt` going from a
    method to a property. Now we can't set `encodedJwt` directly and trust
    it'll work. Instead we have to call `setEncodedJwt`.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import m from 'mithril';
-import { JwtState } from './models/JwtState';
-import { JwtShow } from './views/JwtShow';
-import { ChangelogComponent } from './views/ChangelogComponent';
+import m from 'mithril'
+import { JwtState } from './models/JwtState'
+import { JwtShow } from './views/JwtShow'
+import { ChangelogComponent } from './views/ChangelogComponent'
 
 // It is possible to append a hash and a jwt to jwt.show urls.
-var hash = window.location.hash;
+var hash = window.location.hash
 if (hash) {
-  JwtState.encodedJwt = hash.slice(1); // remove the '#' character.
+  JwtState.setEncodedJwt(hash.slice(1)) // remove the '#' character.
 }
 
 m.route.prefix("")
 m.route(document.body, '/', {
   '/': JwtShow,
   '/changelog': ChangelogComponent,
-});
+})
