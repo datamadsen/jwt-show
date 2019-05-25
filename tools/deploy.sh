7afbfeccdd10b456b3691352ce922d4a2d4a901f
Tim M. Madsen <tim@tmadsen.com>
2019-05-25 18:37:27 +0200

    Add dockerfile
    Prepare jwt-show to be served in a docker container on an environment
    where nginx-proxy and the letsencrypt companion are running. A default
    configuration for nginx is added with the purpose of hitting the
    mithril router in jwt-show if the requested uri does not match a file on
    the server.
    
    We also regenerate yarn.lock and add license to package.json since
    Github found a vulnerability in yarn.lock regarding jquery. Here's to
    hoping it's fixed in the new yarn.lock.

--- tools/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Pahts are relative to "root", where we would normally run `yarn run deploy`.
-yarn
-rm -rf dist
-./tools/generate-changelog-entries.sh > ./src/models/Changes.js
-yarn run build
-sudo -u www-data rm -rf /var/www/jwt.show/*
-sudo -u www-data cp dist/* /var/www/jwt.show
