cd3458d2f71f24c846d1a52f6f80fd06801c7d41
Tim M. Madsen <tim@madsen.fm>
2022-04-13 11:49:52 +0200

    Remove changelog, at least for now

--- src/views/FooterComponent.js
+++ src/views/FooterComponent.js
@@ -6,7 +6,7 @@ export const FooterComponent = {
     return m("div", [
       m("div.mt2", m.trust(`Do you have a request or an issue? <a class="dib" href="https://github.com/tmadsen/jwt-show">Head over to github</a>`)),
       m("div.mt2", m.trust(`Is jwt.show making your life better? <a class="dib" href="https://donorbox.org/support-jwt-show">Show your support</a>`)),
-      m("div.mt2", m.trust(`Are you curious about jwt.show history?&nbsp;&nbsp;<a class="dib" href="/changelog">See changelog</a>`)),
+      // m("div.mt2", m.trust(`Are you curious about jwt.show history?&nbsp;&nbsp;<a class="dib" href="/changelog">See changelog</a>`)),
       m("div.mt3.mb6", m.trust(`Made by <a href="https://datamadsen.dk">datamadsen.dk</a>`))
     ])
   }
